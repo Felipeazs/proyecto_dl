b0d13b4666b403ddba9c91e131ef6ea30ba3bb9f
felipeazs <felipeazs@gmail.com>
2023-02-26 18:00:02 -0300

    arreglo http hook path

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react'
 
-import useHttp from '../hooks/http-hook'
+import useHttp from '../hooks/httpClient-hook'
 
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
